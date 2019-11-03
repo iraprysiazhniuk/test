1d6b76353392c90c6e20b6cdc6f264d7bb6f2d1d
iraprysiazhniuk <38790426+iraprysiazhniuk@users.noreply.github.com>
2019-11-03 19:42:20 +0200

    Update test.js

--- js/test.js
+++ js/test.js
@@ -23,3 +23,23 @@ let setVerifyStatus = (ctx) => {
         task.innerHTML += '<i class="fas fa-check-square"></i>';
     }
 }
+
+let search = document.getElementById('search');
+search.addEventListener('keyup', filterTaskTitles);
+
+function filterTaskTitles(){
+  let searchValue = document.getElementById('search').value.toUpperCase();
+
+  let mainDiv = document.getElementById('tasks');
+  let div = mainDiv.querySelectorAll('div.task');
+
+  for(let i = 0; i < div.length; i++){
+    let p = div[i].getElementsByTagName('p')[0];
+    if(p.innerHTML.toUpperCase().indexOf(searchValue) > -1){
+      div[i].style.display = '';
+    } else {
+      div[i].style.display = 'none';
+    }
+  }
+
+}
