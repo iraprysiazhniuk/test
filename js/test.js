365fa0539d7f7a7de810c8c4cd99c08694592519
iraprysiazhniuk <38790426+iraprysiazhniuk@users.noreply.github.com>
2019-11-03 12:26:33 +0200

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,87 +1,25 @@
-$(document).ready(function(){
-    $("#infoDialog").hide();
-});
-
-function opendialog(){
-    $("#infoDialog").fadeIn();
-}
-function closedialog(){
-    $("#infoDialog").fadeOut();
-}
-function closeDialogAfterTask(){
-    $("#infoDialog").fadeOut();
-}
-$(document).ready(function(){
-    $("#tasks").hide();
-});
-
-function opentasks(){
-    $("#tasks").fadeIn();
-}
-
-function checkParams() {
-    let title = $('#title').val();
-    let description = $('#description').val();
-     
-    if(title.length != 0 && description.length != 0) {
-        $('#dialogSave').removeAttr('disabled');
-    } else {
-        $('#dialogSave').attr('disabled', 'disabled');
-    }
-}
-
-$(function(){
-    $('#dialogForm input[type="submit"]').click(function(e){
-        e.preventDefault();
-
-        let fields = $('#dialogForm').serializeArray();
-        console.log(fields);
-        createNewTaskElement(fields);
+document.addEventListener('DOMContentLoaded', () => {
+    let btnCreate = document.querySelectorAll("[create-task-btn]")[0];
+    let saveBtn = document.querySelector("[save-btn]");
+    let closeBtn = document.querySelector("[cancel-btn]");
+    let dialog;
+    btnCreate.addEventListener('click', event => {
+        dialog = new CreateDialog();
+        dialog.open();
+    });
+    saveBtn.addEventListener('click', event => {
+        dialog.submit();
+    });
+    closeBtn.addEventListener('click', event => {
+        dialog.close();
     });
 
+
 });
-
-var createNewTaskElement=function(taskString){
-
-	let taskTitle=document.createElement("p");
-    taskTitle.className = "taskTitle";
-
-    let descriptionTask=document.createElement("p");
-    descriptionTask.className = "descriptionTask";
-
-    let priority=document.createElement("p");
-    priority.className = "levelTask";
-    
-    let selectAction=document.createElement("div");
-    selectAction.className = "selectAction";
-
-    let select=document.createElement("select");
-    let array = ["...", "done", "edit", "delete"];
-    select.setAttribute("id", "action");
-    selectAction.appendChild(select);
-
-    for(let i = 0; i < array.length; i++){
-        let option = document.createElement("option");
-        option.setAttribute("value", array[i]);
-        option.text = array[i];
-        select.appendChild(option);
+let setVerifyStatus = (ctx) => {
+    let guid = ctx.getAttribute('task');
+    if (ctx.value === 'done') {
+        let task = document.querySelector('[task=' + guid + ']');
+        task.innerHTML += '<i class="fas fa-check-square"></i>';
     }
-    $.each(taskString,function(i,field){
-        if(field.name == "title"){
-            taskTitle.innerText = field.value;
-        }
-        if(field.name == "description"){
-            descriptionTask.innerText = field.value;
-        }
-        if(field.name == "priority"){
-            priority.innerText = field.value;
-        }
-    });
-    let tasks = document.getElementById("tasks");
-    tasks.appendChild(taskTitle);
-    tasks.appendChild(descriptionTask);
-    tasks.appendChild(priority);
-    tasks.appendChild(selectAction);
-    
-
 }
